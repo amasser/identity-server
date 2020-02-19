8f5d3dc62ea2c54317f12229fc5cc479c109b54c
Patrick Pacher <patrick.pacher@gmail.com>
2020-02-19 20:56:32 +0100

    group: added repository definition in iam

--- iam/repositories.go
+++ iam/repositories.go
@@ -2,9 +2,9 @@ package iam
 
 import "context"
 
-// UserRepository provides persistent storage for user account information
+// UserRepository provides persistent storage for user account information.
 type UserRepository interface {
-	// Store stores a user, overwriting and existing one if necassary
+	// Store stores a user, overwriting and existing one if necassary.
 	Store(ctx context.Context, user User) error
 
 	// Delete deletes the user with the given urn. Implementations
@@ -14,9 +14,33 @@ type UserRepository interface {
 	// error code (like 404 Not Found).
 	Delete(ctx context.Context, urn UserURN) error
 
-	// Load returns a read model of the user identified by urn
+	// Load returns a read model of the user identified by urn.
 	Load(ctx context.Context, urn UserURN) (User, error)
 
-	// Get returns a list of read models of all users
+	// Get returns a list of read models of all users.
 	Get(ctx context.Context) ([]User, error)
+}
+
+// GroupRepository provides persistent storage for group information.
+type GroupRepository interface {
+	// Store stores a group, overwritting and existing one if necassary.
+	// Implementation should check the value of group.Members to determine
+	// how to proceed with members. The group.Members slice is only updated
+	// if storeMembers is set to true. Implementation must take care
+	// to copy the old value of group.Members otherwise.
+	Store(ctx context.Context, group Group, storeMembers bool) error
+
+	// Delete deletes an existing account group. Implementations tracking
+	// user-group assignments should clean them up as well. It is not required
+	// for the group to be deleted to exist. If it does not exist, implementations
+	// should either return nil or os.ErrNotExist. Any other error will be treated
+	// as something else and returned back to the user.
+	Delete(ctx context.Context, urn GroupURN) error
+
+	// Load loads the account group from the persistent storage and returns it.
+	// If the group does not exist os.ErrNotFound should be returned.
+	Load(ctx context.Context, urn GroupURN) (Group, error)
+
+	// Get should return all groups from the persistent storage.
+	Get(ctx context.Context) ([]Group, error)
 }
