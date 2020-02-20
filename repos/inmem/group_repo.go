39fb1b85a463684ef248da95084d56fa010ebfb6
Patrick Pacher <patrick.pacher@gmail.com>
2020-02-20 11:26:20 +0100

    repos/inmem: added group repository

---
+++ repos/inmem/group_repo.go
@@ -0,0 +1,63 @@
+package inmem
+
+import (
+	"context"
+	"os"
+	"sync"
+
+	"github.com/tierklinik-dobersberg/identity-server/iam"
+)
+
+type groupRepo struct {
+	rw     sync.RWMutex
+	groups map[iam.GroupURN]iam.Group
+}
+
+// NewGroupRepository creates a new in-memory group repository
+func NewGroupRepository() iam.GroupRepository {
+	return &groupRepo{
+		groups: make(map[iam.GroupURN]iam.Group),
+	}
+}
+
+func (repo *groupRepo) Store(ctx context.Context, group iam.Group) error {
+	repo.rw.Lock()
+	defer repo.rw.Unlock()
+
+	repo.groups[group.ID] = group
+
+	return ctx.Err()
+}
+
+func (repo *groupRepo) Delete(ctx context.Context, urn iam.GroupURN) error {
+	repo.rw.Lock()
+	defer repo.rw.Unlock()
+
+	delete(repo.groups, urn)
+
+	return ctx.Err()
+}
+
+func (repo *groupRepo) Load(ctx context.Context, urn iam.GroupURN) (iam.Group, error) {
+	repo.rw.RLock()
+	defer repo.rw.RUnlock()
+
+	if g, ok := repo.groups[urn]; ok {
+		return g, ctx.Err()
+	}
+
+	return iam.Group{}, os.ErrNotExist
+}
+
+func (repo *groupRepo) Get(ctx context.Context) ([]iam.Group, error) {
+	repo.rw.RLock()
+	defer repo.rw.RUnlock()
+
+	groups := make([]iam.Group, 0, len(repo.groups))
+
+	for _, g := range repo.groups {
+		groups = append(groups, g)
+	}
+
+	return groups, ctx.Err()
+}
