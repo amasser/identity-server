082fa8b8aaf5d28f64686b069960abca1b0ad5fc
Patrick Pacher <patrick.pacher@gmail.com>
2020-02-20 11:26:04 +0100

    iam: removed Groups from User and Members from Group

--- iam/group.go
+++ iam/group.go
@@ -6,8 +6,7 @@ type GroupURN string
 // Group is a collection of user accounts (iam.User) and can be used for
 // authorization
 type Group struct {
-	ID      GroupURN  `json:"id"`
-	Name    string    `json:"name"`
-	Comment string    `json:"comment,omitempty"`
-	Members []UserURN `json:"members,omitempty"`
+	ID      GroupURN `json:"id"`
+	Name    string   `json:"name"`
+	Comment string   `json:"comment,omitempty"`
 }
