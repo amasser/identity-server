f4327575642ea7693abc1fab900ac8bc690de9f7
Patrick Pacher <patrick.pacher@gmail.com>
2020-02-20 18:14:19 +0100

    group: implemented most group service methods

--- services/group/service.go
+++ services/group/service.go
@@ -3,6 +3,7 @@ package group
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/tierklinik-dobersberg/identity-server/iam"
 	"github.com/tierklinik-dobersberg/identity-server/pkg/mutex"
@@ -34,41 +35,110 @@ type Service interface {
 }
 
 type service struct {
-	users user.Service
-	l     *mutex.Mutex
-	repo  iam.GroupRepository
+	users   user.Service
+	l       *mutex.Mutex
+	groups  iam.GroupRepository
+	members iam.MembershipRepository
 }
 
 // NewService returns a new service for managing account group memberships.
 // It depends on having access to the user management service as well as
 // a group repository for persisting changes.
-func NewService(us user.Service, repo iam.GroupRepository) Service {
+func NewService(us user.Service, groups iam.GroupRepository, members iam.MembershipRepository) Service {
 	return &service{
-		users: us,
-		l:     mutex.New(),
-		repo:  repo,
+		users:   us,
+		l:       mutex.New(),
+		groups:  groups,
+		members: members,
 	}
 }
 
 var errNotImplemented = errors.New("not yet implemented")
 
+// ErrInvalidParameter is returned from the group management service if
+// invalid parameters are supplied
+var ErrInvalidParameter = errors.New("invalid parameter")
+
 func (s *service) Create(ctx context.Context, groupName string, groupComment string) (iam.GroupURN, error) {
-	return "", errNotImplemented
+	if groupName == "" {
+		return "", ErrInvalidParameter
+	}
+
+	if !s.l.TryLock(ctx) {
+		return "", ctx.Err()
+	}
+	defer s.l.Unlock()
+
+	grp := iam.Group{
+		ID:      iam.GroupURN(fmt.Sprintf("urn:iam::group/%s", groupName)),
+		Name:    groupName,
+		Comment: groupComment,
+	}
+
+	if err := s.groups.Store(ctx, grp); err != nil {
+		return "", err
+	}
+
+	return grp.ID, nil
 }
 
 func (s *service) Delete(ctx context.Context, urn iam.GroupURN) error {
-	return errNotImplemented
+	if urn == "" {
+		return ErrInvalidParameter
+	}
+
+	if !s.l.TryLock(ctx) {
+		return ctx.Err()
+	}
+	defer s.l.Unlock()
+
+	members, err := s.members.Members(ctx, urn)
+	if err != nil {
+		return err
+	}
+
+	for _, m := range members {
+		if err := s.members.DeleteMember(ctx, m, urn); err != nil {
+			return err
+		}
+	}
+
+	return s.groups.Delete(ctx, urn)
 }
 
-func (s *service) Load(ctx context.Context, urn iam.GroupURN, withMembers bool) (iam.Group, error) {
-	return iam.Group{}, errNotImplemented
+func (s *service) Load(ctx context.Context, urn iam.GroupURN) (iam.Group, error) {
+	if urn == "" {
+		return iam.Group{}, ErrInvalidParameter
+	}
+	if !s.l.TryLock(ctx) {
+		return iam.Group{}, ctx.Err()
+	}
+	defer s.l.Unlock()
+
+	grp, err := s.groups.Load(ctx, urn)
+	return grp, err
 }
 
 func (s *service) UpdateComment(ctx context.Context, urn iam.GroupURN, comment string) error {
-	return errNotImplemented
+	if urn == "" {
+		return ErrInvalidParameter
+	}
+	if !s.l.TryLock(ctx) {
+		return ctx.Err()
+	}
+	defer s.l.Unlock()
+
+	grp, err := s.groups.Load(ctx, urn)
+	if err != nil {
+		return err
+	}
+
+	grp.Comment = comment
+
+	return s.groups.Store(ctx, grp)
 }
 
-func (s *service) AddMember(ctx context.Context, grp iam.GroupURN, memeber iam.UserURN) error {
+func (s *service) AddMember(ctx context.Context, grp iam.GroupURN, member iam.UserURN) error {
 	return errNotImplemented
 }
 
