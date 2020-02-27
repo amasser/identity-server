0ef3c2cdb785a0814c184c5bce628a24fd22ea08
Patrick Pacher <patrick.pacher@gmail.com>
2020-02-27 15:21:46 +0100

    mocks: added policy repository mock

---
+++ mocks/policy_repo.go
@@ -0,0 +1,30 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/tierklinik-dobersberg/identity-server/iam"
+)
+
+type PolicyRepository struct {
+	mock.Mock
+}
+
+func (p *PolicyRepository) Store(ctx context.Context, policy iam.Policy) error {
+	return p.Called(policy).Error(0)
+}
+
+func (p *PolicyRepository) Delete(ctx context.Context, urn iam.PolicyURN) error {
+	return p.Called(urn).Error(0)
+}
+
+func (p *PolicyRepository) Load(ctx context.Context, urn iam.PolicyURN) (iam.Policy, error) {
+	args := p.Called(urn)
+	return args.Get(0).(iam.Policy), args.Error(1)
+}
+
+func (p *PolicyRepository) Get(ctx context.Context) ([]iam.Policy, error) {
+	args := p.Called()
+	return args.Get(0).([]iam.Policy), args.Error(1)
+}
