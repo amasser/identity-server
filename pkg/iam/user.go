940535cc981b97e4536275cb45495d0c6a1ae4b8
Patrick Pacher <patrick.pacher@gmail.com>
2020-02-29 09:24:39 +0100

    iam: Added utility methods for UserURN

--- pkg/iam/user.go
+++ pkg/iam/user.go
@@ -1,7 +1,52 @@
 package iam
 
+import "strings"
+
 // UserURN uniquely identifies a paticular user/account
 type UserURN string
+
+// IsValid returns true if the URN is a valid IAM User URN. False
+// otherwise.
+func (urn UserURN) IsValid() bool {
+	return strings.HasPrefix(string(urn), "urn:iam::user/")
+}
+
+// Path returns the last part of the URN. For UserURN, that is
+// user/<account-id>
+func (urn UserURN) Path() string {
+	if !urn.IsValid() {
+		return ""
+	}
+
+	parts := strings.Split(string(urn), ":")
+	path := parts[3]
+	return path
+}
+
+// SubType returns the subtype of the resource
+func (urn UserURN) SubType() string {
+	path := urn.Path()
+	if path == "" {
+		return ""
+	}
+
+	parts := strings.Split(path, "/")
+	return parts[0]
+}
+
+// AccountID returns the account ID for the user.
+func (urn UserURN) AccountID() string {
+	path := urn.Path()
+	if path == "" {
+		return ""
+	}
+
+	parts := strings.Split(path, "/")
+	if len(parts) != 2 {
+		return ""
+	}
+	return parts[1]
+}
 
 // User represents a user account managed by IAM and authenticated by
 // authn-server
